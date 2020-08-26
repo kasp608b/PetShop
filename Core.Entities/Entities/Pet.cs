a97b2da3d10c65e9ba1d99e10ddd87606ff3a20a
kasp608b <54952896+kasp608b@users.noreply.github.com>
2020-08-26 11:00:29 +0200

    uff

---
+++ Core.Entities/Entities/Pet.cs
@@ -0,0 +1,19 @@
+﻿using PetShop.Core.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.Entities
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public PetType Type { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime SoldDate { get; set; }
+        public string Color { get; set; }
+        public string PreviousOwner { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Core.Entities/Enums/PetType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.Entities.Enums
+{
+    public enum PetType
+    {
+        CAT,
+        DOG,
+        FISH,
+        GOAT,
+        TIGER,
+        PARROT,
+
+    }
+}
