49b66a71e86e2493197d73a49ef8e43af41d8abb
kasp608b <54952896+kasp608b@users.noreply.github.com>
2020-08-26 10:25:33 +0200

    Initial commit

---
+++ PetShop.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PetShop.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
